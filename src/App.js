724225112391d2f2567d520e540f0fed9d86af10
sooyoung-h <sooyoung.h8@gmail.com>
2021-02-17 17:45:34 +0900

    Home > modal 창 수정
    부모-자식1(배경), 자식2(모달창)
    fixed - absolute - absolute

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import { Nav, Jumbotron  } from "react-bootstrap";
-import { Link, Route } from 'react-router-dom';
-import { useState } from 'react';
-import { propTypes } from 'react-bootstrap/esm/Image';
+import logo from "./logo.svg";
+import "./App.css";
+import { Nav, Jumbotron } from "react-bootstrap";
+import { Link, Route } from "react-router-dom";
+import { useState } from "react";
+import { propTypes } from "react-bootstrap/esm/Image";
 import { connect } from "react-redux";
 import Detail from "./Detail.js";
 import Cart from "./Cart.js";
@@ -11,19 +11,27 @@ import Cart from "./Cart.js";
 function Modal(props) {
   //하위로 컴포넌트를 뺴놓을 때마다 태그 속성으로 보내주고 props.( )로 받기!
   return (
-    <div className="detailModal">
-      <img src={props.state[props.num].path}></img>
-      <h4 className="item__name" >{props.state[props.num].name}</h4>
-      <p className="item__content" >{props.state[props.num].content}</p>
-      <p className="item__price" >{props.state[props.num].price}원</p>
-      <button className="item__detailBtn smallBtn">
-        <Link to={"/detail/"+ props.num}>See more</Link>
+    <div className="modal">
+      <div className="modal__bg"></div>
+      <div className="modal__fg">
+        <img src={props.state[props.num].path}></img>
+        <h4 className="item__name">{props.state[props.num].name}</h4>
+        <p className="item__content">{props.state[props.num].content}</p>
+        <p className="item__price">{props.state[props.num].price}원</p>
+        <Link className="item__detailBtn smallBtn" to={"/detail/" + props.num}>
+          See more
+        </Link>
+        <button
+          className="item__detailBtn smallBtn"
+          onClick={() => {
+            props.modalChange(false);
+          }}
+        >
+          Close
         </button>
-      <button className="item__detailBtn smallBtn" onClick={()=>{
-        props.modalChange(false);
-      }}>Close</button>
+      </div>
     </div>
-  )
+  );
 }
 
 function Home(props) {
@@ -32,71 +40,72 @@ function Home(props) {
   let [modal, modalChange] = useState(false);
   let [num, numChange] = useState(0);
 
-  
-  return(
-      <div className="home">
-        
+  return (
+    <div className="home">
+      <Jumbotron className="background">
+        <span className="jumbo__title">Enjoy your dessert time</span>
+        <p className="jumbo__content">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+      </Jumbotron>
 
-        <Jumbotron className="background">
-          <span className="jumbo__title" >Enjoy your dessert time</span>
-          <p className="jumbo__content" >
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-          </p>
-        </Jumbotron>
+      <div className="divider">Dessert makes you happy</div>
 
-        <div className="divider">
-          Dessert makes you happy
-        </div>
-
-        <div className="items">
-          {
-            props.state.map((a,i)=>{
-              return (
-                <div className="items__item" >
-                  <img src={props.state[i].path}></img>
-                  <h4 className="item__name" >{props.state[i].name}</h4>
-                  <p className="item__content" >{props.state[i].content}</p>
-                  <p className="item__price" >{props.state[i].price}원</p>
-                  <button className="item__detailBtn smallBtn" onClick={()=>{
-                    modalChange(true);
-                    numChange(i);
-                  }}>Detail</button>
-                  <button className="item__addBtn smallBtn" onClick={()=>{
-                    console.log('add button');
-                  }}>Add</button>
-                  
-                </div>
-              )
-            })
-          }
-        </div>
-        {
-            modal === true
-            ? <Modal state={props.state} num={num} modalChange={modalChange} ></Modal>
-            : null
-        }
-
-        <footer>
-          contact@food.make.you.happy
-        </footer>
+      <div className="items">
+        {props.state.map((a, i) => {
+          return (
+            <div className="items__item">
+              <img src={props.state[i].path}></img>
+              <h4 className="item__name">{props.state[i].name}</h4>
+              <p className="item__content">{props.state[i].content}</p>
+              <p className="item__price">{props.state[i].price}원</p>
+              <button
+                className="item__detailBtn smallBtn"
+                onClick={() => {
+                  modalChange(true);
+                  numChange(i);
+                }}
+              >
+                Detail
+              </button>
+              <button
+                className="item__addBtn smallBtn"
+                onClick={() => {
+                  console.log("add button");
+                }}
+              >
+                Add
+              </button>
+            </div>
+          );
+        })}
       </div>
-    
-  )
+      {modal === true ? (
+        <Modal state={props.state} num={num} modalChange={modalChange}></Modal>
+      ) : null}
+
+      <footer>contact@food.make.you.happy</footer>
+    </div>
+  );
 }
 
 function App(props) {
   return (
     <div className="App">
       <header>
-          <span className="dessert" >Dessert</span>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
-          </ul>
-        </header>
+        <span className="dessert">Dessert</span>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </header>
       <Route exact path="/">
-        <Home state={props.state} ></Home>
+        <Home state={props.state}></Home>
         {/* 리덕스로 받아온 스테이트를 다시 하위컴포넌트로 전달해줄 경우 */}
       </Route>
       <Route path="/detail/:id">
@@ -109,8 +118,8 @@ function App(props) {
   );
 }
 
-function myFunc(state){
-  return {state : state}
+function myFunc(state) {
+  return { state: state };
 }
 
 export default connect(myFunc)(App);
